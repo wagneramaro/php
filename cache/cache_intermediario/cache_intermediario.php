860e31983b572553d10eac52b8db44f0b323f7fa
Wagner Amaro (PC Certa) <wagner@certasolucoes.com>
2019-07-01 09:55:11 -0300

    arquivos iniciais

---
+++ cache/cache_intermediario/cache_intermediario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cache Intermediário</title>
+</head>
+<body>
+
+	<div style="width:300px; margin:auto; background-color: #999; padding: 30px;">
+	<h1>Este é um cabeçalho <?php echo rand(0 ,9999); ?></h1>
+	</div>
+
+	<form method="POST">
+		<input type="text" name="" placeholder="E-mail"><br><br>
+		<input type="submit" value="Entrar">
+
+	</form>
+
+</body>
+</html>
