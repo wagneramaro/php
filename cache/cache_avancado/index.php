64f10ff2db890979e9690ba80e3429f6b42773e2
Wagner Amaro (PC Certa) <contato@agenciawide.com.br>
2019-07-08 09:02:55 -0300

    Cache acançado funcionando.

--- cache/cache_avancado/index.php
+++ cache/cache_avancado/index.php
@@ -1,10 +1,21 @@
 <?php 
+function is_valido($cache){
+	$ultima_modificacao = filemtime($cache);
+	$c = time() - $ultima_modificacao;
+
+	if($c > 15){
+		return false;
+	} else {
+		return true;
+	}
+}
+
 $p = 'pagina';
 if(isset($_GET['p']) && !empty($_GET['p']) && file_exists('paginas/'.$_GET['p'].'.php')){
 	$p = $_GET['p'];
 }
 
-if (file_exists('caches/'.$p.'.cache')) {
+if (file_exists('caches/'.$p.'.cache') && is_valido('caches/'.$p.'.cache')) {
 	require 'caches/'.$p.'.cache';
 } else {
 	ob_start();
