9acd03649932ef31138df30c6eb3013261804249
Wagner Amaro (PC Certa) <contato@agenciawide.com.br>
2019-07-08 08:22:46 -0300

    Cache por Páginas

--- cache/cache_avancado/index.php
+++ cache/cache_avancado/index.php
@@ -1,13 +1,18 @@
 <?php 
-if (file_exists('cache.intermediario')) {
-	require 'cache.intermediario';
+$p = 'pagina';
+if(isset($_GET['p']) && !empty($_GET['p']) && file_exists('paginas/'.$_GET['p'].'.php')){
+	$p = $_GET['p'];
+}
+
+if (file_exists('caches/'.$p.'.cache')) {
+	require 'caches/'.$p.'.cache';
 } else {
 	ob_start();
-	require 'cache_intermediario.php';
+	require 'paginas/'.$p.'.php';
 	$html = ob_get_contents();
 	ob_end_clean();
 
-	file_put_contents('cache.intermediario', $html);
+	file_put_contents('caches/'.$p.'.cache', $html);
 	echo $html;
 }
 
